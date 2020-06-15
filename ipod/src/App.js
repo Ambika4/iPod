431ea7dd247f58442eba8ebb378d79083abab1f9
Ambika Kumari <kambika1309@gmail.com>
2020-06-15 20:23:40 +0530

    wheel

---
+++ ipod/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Wheel from './Wheel/wheel';
+import Menu from './Menu/Menu';
+function App() {
+  return (
+    <div className="App">
+      <Menu/>
+      <Wheel/>
+    </div>
+  );
+}
+
+export default App;
---
+++ ipod/src/Wheel/wheel.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import styles from './wheel.module.css'; 
+import 'font-awesome/css/font-awesome.min.css';
+import ZingTouch from 'zingtouch';
+
+const Wheel=()=>{
+  function rotatewheel(e){
+    var currentAngle = 15;
+    
+    var target = document.getElementById('wheel-square-id');
+    var region = new ZingTouch.Region(e.target);
+   
+    region.bind(target, 'rotate', function(e) {
+    //   var rotatable = document.getElementById('rotatable');
+      currentAngle += e.detail.distanceFromLast;
+      console.log(currentAngle);
+    
+    });
+    
+
+  }
+   return(
+       <div onClick={rotatewheel}className="wheel-square"id="wheel-square-id"> 
+        
+        <div className={styles.toggle}></div>
+        <div id="rotatable">
+        <div  className={styles.menu}>
+        <div>Menu</div>
+        <div><i className="fa fa-fast-forward" aria-hidden="true"></i></div>
+        <div><i className="fa fa-fast-backward" aria-hidden="true"></i></div>
+        <div><i className="fa fa-pause-circle-o" aria-hidden="true"></i></div>
+       
+        </div>
+      
+        </div>
+       
+       </div>
+   )
+}
+export default Wheel;
